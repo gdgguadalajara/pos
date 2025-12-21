303ba3dae2ea64f4bdd569314f918bf0b8b52536
ushieru <uzielcocolan@gmail.com>
2025-12-21 14:37:29 -0600

    Feat: enhance GlobalExceptionMapper to include UriInfo context for API path checks

--- src/main/java/com/gdgguadalajara/pos/common/exception/GlobalExceptionMapper.java
+++ src/main/java/com/gdgguadalajara/pos/common/exception/GlobalExceptionMapper.java
@@ -1,18 +1,26 @@
 package com.gdgguadalajara.pos.common.exception;
 
 import com.gdgguadalajara.pos.common.model.ErrorResponse;
-
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
 public class GlobalExceptionMapper implements ExceptionMapper<Exception> {
 
+    @Context
+    UriInfo uriInfo;
+
     @Override
     public Response toResponse(Exception e) {
-        int status = (e instanceof jakarta.ws.rs.WebApplicationException wae)
+        if (!uriInfo.getPath().startsWith("/api")) {
+            return null;
+        }
+        int status = (e instanceof WebApplicationException wae)
                 ? wae.getResponse().getStatus()
                 : 500;
 
