f1ab33f2b298a0a50908755310ea9d590dd5752d
ushieru <uzielcocolan@gmail.com>
2025-12-23 21:19:14 -0600

    Feat: enhance ticket display logic to show message when no tickets are pending

--- src/main/webui/app/pages/cashier/tickets/index.vue
+++ src/main/webui/app/pages/cashier/tickets/index.vue
@@ -15,13 +15,19 @@ const { data: paginatedTickets } = useAsyncData('myTickets', () =>
         <NuxtLayout name="cashier" title="Tickets">
             <div class="card bg-base-200">
                 <div class="card-body grid grid-cols-3">
-                    <NuxtLink :to="`/cashier/tickets/${ticket.id}`" class="card bg-base-100 cursor-pointer"
-                        v-for="ticket in paginatedTickets?.data" :key="ticket.id">
+                    <NuxtLink v-if="paginatedTickets?.data.length" :to="`/cashier/tickets/${ticket.id}`"
+                        class="card bg-base-100 cursor-pointer" v-for="ticket in paginatedTickets?.data"
+                        :key="ticket.id">
                         <div class="card-body">
                             <span class="text-center">#{{ ticket.id }}
                             </span>
                         </div>
                     </NuxtLink>
+                    <div v-else class="card">
+                        <div class="card-body">
+                            <span class="text-center">Sin tickets pendientes...</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </NuxtLayout>
