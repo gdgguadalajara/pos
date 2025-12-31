68ff403de2a55303f00785653bd43e7a3c1544dc
ushieru <uzielcocolan@gmail.com>
2025-12-31 14:16:53 -0600

    Feat: add table tableSnapshot for auditing

--- src/main/java/com/gdgguadalajara/pos/ticket/model/Ticket.java
+++ src/main/java/com/gdgguadalajara/pos/ticket/model/Ticket.java
@@ -1,5 +1,6 @@
 package com.gdgguadalajara.pos.ticket.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.gdgguadalajara.pos.floorplan.model.RestaurantTable;
 import com.gdgguadalajara.pos.ticketItem.model.TicketItem;
@@ -7,12 +8,15 @@ import com.gdgguadalajara.pos.user.model.User;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Entity
@@ -30,6 +34,11 @@ public class Ticket extends PanacheEntityBase {
     @JoinColumn(name = "table_id", nullable = true)
     public RestaurantTable table;
 
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    @JsonIgnore
+    public Map<String, Object> tableSnapshot;
+
     @Column(nullable = false)
     public Integer dinerCount = 1;
 
@@ -39,7 +48,7 @@ public class Ticket extends PanacheEntityBase {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     public TicketStatus status;
-    
+
     @JsonManagedReference
     @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL, orphanRemoval = true)
     public List<TicketItem> items = new ArrayList<>();
