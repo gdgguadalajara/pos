8ff62f25dbc66c318e2f9348f48d32425c906787
ushieru <uzielcocolan@gmail.com>
2026-01-02 11:18:30 -0600

    Fix: calculate change given

--- src/main/java/com/gdgguadalajara/pos/payment/application/CreatePayment.java
+++ src/main/java/com/gdgguadalajara/pos/payment/application/CreatePayment.java
@@ -23,13 +23,14 @@ public class CreatePayment {
         var ticket = Ticket.<Ticket>findById(ticketid);
         if (ticket == null)
             throw DomainException.notFound("Ticket no encontrado");
-        if(ticket.items.size() == 0)
+        if (ticket.items.size() == 0)
             throw DomainException.badRequest("El ticket no tiene items");
         payment.ticket = ticket;
         payment.amount = createPaymentRequest.amount();
         payment.method = createPaymentRequest.method();
         payment.externalReference = createPaymentRequest.externalReference();
-        payment.changeGiven = ticket.totalAmount.subtract(createPaymentRequest.amount());
+        if (createPaymentRequest.amount().compareTo(ticket.totalAmount) > 0)
+            payment.changeGiven = createPaymentRequest.amount().subtract(ticket.totalAmount);
         payment.persistAndFlush();
         var payments = Payment.<Payment>find("ticket.id", ticket.id).list();
         var totalPayments = payments.stream().map(p -> p.amount).reduce(BigDecimal.ZERO, BigDecimal::add);
