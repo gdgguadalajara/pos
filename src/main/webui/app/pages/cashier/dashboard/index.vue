4049d56cc1b7334ccd2099fe3ce0e6d1e5f44b95
ushieru <uzielcocolan@gmail.com>
2026-01-03 00:29:26 -0600

    Chore: add cashier dashboard

---
+++ src/main/webui/app/pages/cashier/dashboard/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+definePageMeta({
+    middleware: ['only-cashier'],
+})
+</script>
+
+<template>
+    <div>
+        <NuxtLayout name="cashier" title="Dashboard">
+            <WorkInProgress />
+        </NuxtLayout>
+    </div>
+</template>
