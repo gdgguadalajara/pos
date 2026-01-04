7545d74c1ad403ebe3010a0a9239f574b6b456af
ushieru <uzielcocolan@gmail.com>
2026-01-04 17:14:14 -0600

    Refactor: add products to a category

--- src/main/webui/app/pages/admin/categories/[id]/products.vue
+++ src/main/webui/app/pages/admin/categories/[id]/products.vue
@@ -9,27 +9,42 @@ definePageMeta({
 
 const toast = useToast()
 const route = useRoute()
+const { params, setParam } = useParams('adminGetApiProductsInCategories', { size: 100 })
 const categoryId = route.params.id
 
 const title = ref('Productos en categoria')
-const search = ref('')
 
 const { data: category, status } =
-    useLazyAsyncData(categoryId, () => getApiCategoriesUuid(categoryId))
+    useLazyAsyncData(`getApiCategoriesUuid-${categoryId}`, () => getApiCategoriesUuid(categoryId))
 
 const { data: categoryProducts, refresh: refreshCategoryProducts } =
-    useLazyAsyncData(`${categoryId}-products`, () => getApiCategoriesCategoryIdProducts(categoryId), {
+    useLazyAsyncData(`getApiCategoriesCategoryIdProducts-${categoryId}`, () => getApiCategoriesCategoryIdProducts(categoryId), {
         immediate: false,
         default: () => []
     })
 
 const { data: products, status: productsStatus, refresh: refreshProducts } =
-    useLazyAsyncData('products', () => getApiProducts({
-        size: 100
-    }), {
+    useLazyAsyncData('getApiProducts', () => getApiProducts(params.value), {
         immediate: false,
         default: () => []
     })
+
+const addCategory = (product) => postApiCategoriesCategoryIdProductsProductId(categoryId, product.id)
+    .then(_ => toast.info({ message: 'Producto agregado a la categoria' }))
+    .then(_ => refreshCategoryProducts())
+    .catch(_ => toast.error({ message: 'Error al agregar el producto a la categoria' }))
+
+const removeCategory = (product) => deleteApiCategoriesCategoryIdProductsProductId(categoryId, product.id)
+    .then(_ => toast.info({ message: 'Producto eliminado de la categoria' }))
+    .then(_ => refreshCategoryProducts())
+    .catch(_ => toast.error({ message: 'Error al eliminar el producto de la categoria' }))
+
+const onSubmitSearch = (e) => setParam('name', e.target.name.value)
+
+const isProductInCategory = (product) =>
+    categoryProducts.value.some(categoryProduct => categoryProduct.id == product.id)
+
+watch(params, () => refreshProducts())
 
 watch(status, () => {
     if (status.value == 'error') {
@@ -42,51 +57,39 @@ watch(status, () => {
         refreshCategoryProducts()
     }
 })
-
-const addCategory = (product) => postApiCategoriesCategoryIdProductsProductId(categoryId, product.id)
-    .then(() => {
-        toast.info({ message: 'Producto agregado a la categoria' })
-        refreshCategoryProducts()
-    })
-    .catch(() => toast.error({ message: 'Error al agregar el producto a la categoria' }))
-
-const removeCategory = (product) => deleteApiCategoriesCategoryIdProductsProductId(categoryId, product.id)
-    .then(() => {
-        toast.info({ message: 'Producto eliminado de la categoria' })
-        refreshCategoryProducts()
-    })
-    .catch(() => {
-        toast.error({ message: 'Error al eliminar el producto de la categoria' })
-    })
-
-const isProductInCategory = (product) => {
-    return categoryProducts.value.some(categoryProduct => categoryProduct.id == product.id)
-}
 </script>
 
 <template>
     <div>
         <NuxtLayout name="admin" :title="title">
-            <template v-if="status == 'pending' || productsStatus == 'pending'">
+            <template v-if="status == 'pending'">
                 <div class="grid place-items-center">
                     <span class="loading loading-ring loading-xl"></span>
                 </div>
             </template>
-            <template v-if="status == 'success' && productsStatus == 'success'">
+            <template v-if="status == 'success'">
                 <div class="card bg-base-200">
                     <div class="card-body">
                         <h3 class="card-title">Productos</h3>
-                        <fieldset class="fieldset">
-                            <legend class="fieldset-legend">Buscar</legend>
-                            <input v-model="search" type="text" class="input w-full"
-                                placeholder="🔍 Buscar por nombre" />
-                        </fieldset>
+                        <form class="flex items-end gap-2 mb-3" @submit.prevent="onSubmitSearch">
+                            <fieldset class="fieldset flex-1 p-0">
+                                <legend class="fieldset-legend">Buscar por nombre</legend>
+                                <input type="text" class="input w-full" name="name"
+                                    placeholder="🔍 Buscar por nombre" />
+                            </fieldset>
+                            <button class="btn btn-primary flex-none px-10">
+                                <Icon name="material-symbols:search" class="text-xl" />
+                                Buscar
+                            </button>
+                        </form>
                         <div class="divider"></div>
-                        <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-8 gap-4">
-                            <template
-                                v-for="product in
-                                    products.data.filter(product => product.name.toLowerCase().startsWith(search.toLowerCase()))"
-                                :key="product.id">
+                        <div v-if="productsStatus == 'pending'" class="grid place-items-center">
+                            <span class="loading loading-ring loading-xl"></span>
+                        </div>
+                        <div v-if="productsStatus == 'success'"
+                            class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-8 gap-4">
+                            <template v-for="product in
+                                products.data" :key="product.id">
                                 <button v-if="isProductInCategory(product)"
                                     class="aspect-square grid place-content-center rounded-lg bg-primary cursor-pointer"
                                     @click="removeCategory(product)">
