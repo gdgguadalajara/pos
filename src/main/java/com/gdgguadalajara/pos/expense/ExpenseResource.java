3a9d54e439a49d41f20253d9e6e1febf157f5f9c
ushieru <uzielcocolan@gmail.com>
2026-01-05 12:09:39 -0600

    Feat: add expense filters

--- src/main/java/com/gdgguadalajara/pos/expense/ExpenseResource.java
+++ src/main/java/com/gdgguadalajara/pos/expense/ExpenseResource.java
@@ -1,22 +1,20 @@
 package com.gdgguadalajara.pos.expense;
 
 import com.gdgguadalajara.pos.account.model.AccountRole;
-import com.gdgguadalajara.pos.common.PageBuilder;
 import com.gdgguadalajara.pos.common.model.PaginatedResponse;
+import com.gdgguadalajara.pos.common.util.PanacheCriteria;
 import com.gdgguadalajara.pos.expense.application.CreateExpense;
 import com.gdgguadalajara.pos.expense.model.Expense;
 import com.gdgguadalajara.pos.expense.model.dto.CreateExpenseRequest;
+import com.gdgguadalajara.pos.expense.model.dto.ReadExpensesFilter;
 
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Positive;
-import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
 import lombok.AllArgsConstructor;
 
 @Path("/api/expenses")
@@ -27,10 +25,18 @@ public class ExpenseResource {
 
     @GET
     @RolesAllowed({ AccountRole.ADMIN_ROLE, AccountRole.CASHIER_ROLE })
-    public PaginatedResponse<Expense> read(
-            @QueryParam("page") @DefaultValue("1") @Positive @Valid Integer page,
-            @QueryParam("size") @DefaultValue("10") @Positive @Max(100) @Valid Integer size) {
-        return PageBuilder.of(Expense.findAll(), page, size);
+    public PaginatedResponse<Expense> read(@BeanParam @Valid ReadExpensesFilter filter) {
+        var criteria = PanacheCriteria.of(Expense.class)
+                .eq("id", filter.id)
+                .like("createdBy.name", filter.createdBy)
+                .eq("category", filter.category)
+                .ge("amount", filter.amount)
+                .page(filter.page, filter.size)
+                .orderBy(filter.sort);
+        if (filter.createdAt != null)
+            criteria.gt("createdAt", filter.createdAt.atStartOfDay())
+                    .lt("createdAt", filter.createdAt.plusDays(1).atStartOfDay());
+        return criteria.getResult();
     }
 
     @POST
