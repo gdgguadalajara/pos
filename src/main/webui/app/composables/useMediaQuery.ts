8dcc438b7ed6e9dd254c70daa1ac65bd56cf1bb8
ushieru <uzielcocolan@gmail.com>
2026-01-08 12:45:30 -0600

    Refactor: improve state management in useMediaQuery

--- src/main/webui/app/composables/useMediaQuery.ts
+++ src/main/webui/app/composables/useMediaQuery.ts
@@ -4,7 +4,7 @@
  */
 export default function (query: string) {
     const mediaQueryList = window.matchMedia(query);
-    const matches = ref(mediaQueryList.matches)
+    const matches = useState(query, () => mediaQueryList.matches)
     const handleScreenChange = (event: MediaQueryListEvent) => matches.value = event.matches
     mediaQueryList.addEventListener("change", handleScreenChange, { passive: true })
     return computed(() => matches.value);
