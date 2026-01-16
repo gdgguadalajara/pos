4943d60202ad4a1e2d985a57bb6201b4b647746a
ushieru <uzielcocolan@gmail.com>
2026-01-16 15:44:56 -0600

    Fix: postgres migrations

--- src/main/resources/db/migration/postgresql/V1.0.6__Create_Ingredient.sql
+++ src/main/resources/db/migration/postgresql/V1.0.6__Create_Ingredient.sql
@@ -1,15 +1,9 @@
 create table Ingredient (
-	costPerPurchaseUnit float(53) not null,
-	systemStock float(53) not null,
-	id uuid not null,
-	recipe_id uuid,
-	name varchar(255) not null,
+    costPerPurchaseUnit float(53) not null,
+    systemStock float(53) not null,
+    id uuid not null,
+    name varchar(255) not null,
     purchaseUnit varchar(255) not null check ((purchaseUnit in ('TON','KG','G','MG','L','ML','PIECE'))),
     usageUnit varchar(255) not null check ((usageUnit in ('TON','KG','G','MG','L','ML','PIECE'))),
-	primary key (id)
+    primary key (id)
 );
-
-alter table if exists Ingredient 
-	add constraint FKe6shif331ud2cn5gppjcd7rjv 
-	foreign key (recipe_id) 
-	references Recipe;
