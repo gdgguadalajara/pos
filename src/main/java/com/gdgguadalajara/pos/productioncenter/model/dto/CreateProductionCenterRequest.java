42a446d8b75c3ffc881492c67a24e72777ea3d80
bany-ph <bpuertaherrera@gmail.com>
2026-01-17 21:16:38 -0500

    feat: Add jakarta bean validation annotations to `CreateProductionCenterRequest` and `CreateTicketRequest` fields

--- src/main/java/com/gdgguadalajara/pos/productioncenter/model/dto/CreateProductionCenterRequest.java
+++ src/main/java/com/gdgguadalajara/pos/productioncenter/model/dto/CreateProductionCenterRequest.java
@@ -1,8 +1,16 @@
 package com.gdgguadalajara.pos.productioncenter.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CreateProductionCenterRequest(
+        @NotBlank
         String name,
+        @NotEmpty
         String description,
+        @NotNull
         Boolean isActive) {
 
 }
