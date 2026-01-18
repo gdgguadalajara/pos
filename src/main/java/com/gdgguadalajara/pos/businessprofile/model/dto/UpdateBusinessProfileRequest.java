587d7df8a860e3628ced24d171fa74139c28f823
bany-ph <bpuertaherrera@gmail.com>
2026-01-17 19:05:22 -0500

    feat: Add jakarta bean validation annotations to `UpdateBusinessProfileRequest` fields

--- src/main/java/com/gdgguadalajara/pos/businessprofile/model/dto/UpdateBusinessProfileRequest.java
+++ src/main/java/com/gdgguadalajara/pos/businessprofile/model/dto/UpdateBusinessProfileRequest.java
@@ -1,13 +1,34 @@
 package com.gdgguadalajara.pos.businessprofile.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record UpdateBusinessProfileRequest(
+        @NotBlank
         String name,
+        @NotBlank
         String legalName,
+        @NotBlank
         String taxId,
+        @NotBlank
         String address,
+        /*
+         * ITU E.123 and E.164 phone number formats
+         * */
+        @Pattern(regexp = "^\\+\\d{1,3}\\s\\d{2,3}\\s\\d{2,3}\\s\\d{4}|^\\+\\d{1,3}\\s\\d{1,14}(\\s\\d{1,13})?|^\\(\\d{3}\\)\\s\\d{3}\\s\\d{4}?")
         String phone,
+        @Email
+        @NotBlank
         String email,
+
+        /* pattern can be empty ↓*/
+        @Pattern(regexp = "^(?:(https:\\\\/\\\\/www\\\\.|http:\\\\/\\\\/www\\\\.|https:\\\\/\\\\/|http:\\\\/\\\\/)?[a-zA-Z0-9]{2,}(\\\\.[a-zA-Z0-9]{2,})(\\\\.[a-zA-Z0-9]{2,})?)?$")
         String website,
+
         String ticketFooter,
-        String logoUrl) {
-}
+        @Pattern(regexp = "^(?:(https:\\\\/\\\\/www\\\\.|http:\\\\/\\\\/www\\\\.|https:\\\\/\\\\/|http:\\\\/\\\\/)?[a-zA-Z0-9]{2,}(\\\\.[a-zA-Z0-9]{2,})(\\\\.[a-zA-Z0-9]{2,})?)?$")
+        String logoUrl
+)
+{}
